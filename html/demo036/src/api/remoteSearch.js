d8521e785a1abaaf96069856bf4532de55745026
孙超 <2061857679@qq.com>
2018-11-10 12:14:07 +0800

    b

--- html/demo036/src/api/remoteSearch.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function userSearch(name) {
-  return request({
-    url: 'http://localhost:8080/search/user',
-    method: 'get',
-    params: { name }
-  })
-}
